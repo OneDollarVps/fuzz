883bc86a44e122c06af54ab498ad0efeedab6186
OneDollarVps <131561697+OneDollarVps@users.noreply.github.com>
2023-04-23 01:22:55 +0200

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,1 +1,9 @@
-imp
+import requests
+import time
+file = open("commun.txt","r")
+file = file.readlines()
+while(True):
+  time.sleep(1)
+  for line in file:
+    res= requests.get("https://https://wix.com/"+line.rstrip("\r\n"))
+    print("req code:\t"+str(res.status_code)+"\t length:\t"+str(len(res.text))
