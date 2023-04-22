20b8717404163259982a84df51bf9305ec622705
OneDollarVps <131561697+OneDollarVps@users.noreply.github.com>
2023-04-23 01:27:41 +0200

    Update fuzz.py

--- fuzz.py
+++ fuzz.py
@@ -1,9 +1,12 @@
 import requests
 import time
-file = open("commun.txt","r")
+
+file = open("commun.txt", "r")
 file = file.readlines()
-while(True):
-  time.sleep(1)
-  for line in file:
-    res= requests.get("https://https://wix.com/"+line.rstrip("\r\n"))
-    print("req code:\t"+str(res.status_code)+"\t length:\t"+str(len(res.text))
+x = 0
+while true:
+    time.sleep(1)
+    for line in file:
+        res = requests.get("https://https://wix.com/" + line.rstrip("\r\n"))
+        x = x + 1
+        print("req code:\t" + str(res.status_code) + "N:\t" + str(x))
